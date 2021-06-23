12614d1cd5ede0266471091e6b5c9e4a0dbe64cd
haisum <haisumbhatti@gmail.com>
2021-06-22 22:51:55 -0400

    add mysql code in index.php

--- docker/php/src/index.php
+++ docker/php/src/index.php
@@ -1,3 +1,26 @@
 <?php
-phpinfo();
-?>
+$servername = getenv("DB_HOST");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$dbname = getenv("DB_NAME");
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "show tables;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo $row["Tables_in_$dbname"] . "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?> 
